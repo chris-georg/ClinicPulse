55a15fa927167c771a683aeee22b26527ddb3d3b
Chris Geroge <chrisgeorge042@gmail.com>
2026-07-25 19:17:54 +0100

    Update

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -6,8 +6,8 @@ export default async function handler(
   res: VercelResponse
 ) {
   const supabase = createClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+    process.env.VITE_SUPABASE_URL!,
+    process.env.VITE_SUPABASE_ANON_KEY!
   );
 
   const { data: posts, error } = await supabase
